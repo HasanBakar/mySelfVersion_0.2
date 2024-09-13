b6e41dce835b6140cf29293f1ab0e553421f373c
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2024-09-14 04:38:33 +0600

    arrary define and initialization :time:

---
+++ First Milestone/arrayDefine.cpp
@@ -0,0 +1,34 @@
+//✅
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int numbers [5] = {1,2,3,4,5}; // declaration and initialization
+//     for(int i=0;i<5;i++){ // accessing array elements using loop
+//         cout<<numbers[i]<<endl; 
+//     }
+//     return 0;
+// }
+
+//✅
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string friends[4] = {"Akib","Rahim","Sakib","Rahman"}; // declaration and initialization
+//     for(int i=0;i<4;i++){ // accessing array elements using loop
+//         cout<<friends[i]<<endl; 
+//     }
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i=0;
+    char letters [7] = {'a','b','c','d','e','f','g'}; // declaration and initialization
+    while (i<7)
+    {
+        cout<<letters[i]<<endl;
+        i++;
+    }
+    
+}
