3d8384f866b826b73cabb909267c168522e2b2f8
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2024-09-15 03:05:25 +0600

    :new: variable, keywords, Identifieers, data types

--- documentationStudy&Practice/comments.cpp
+++ documentationStudy&Practice/comments.cpp
@@ -5,7 +5,9 @@ Author: [Abu Bakar](https://github.com/HasanBakar)
 #include<iostream>
 using namespace std;
 int main(){
+    int a; // declaration a variable
+    a = 10; // initialization a variable with value 10
     // print hello world on screen
-    cout << "Hello, World!" << endl;
+    cout << "Hello, World! " << a << endl;// accessing a variable and displaying it on screen
     return 0;
 }
