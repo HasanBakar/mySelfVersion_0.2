75ed9e40895b5ecaaa8b40872ccea3b11ed82fbf
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2024-09-15 02:25:21 +0600

    :new: comments concepts clear :star: comment topic done

---
+++ documentationStudy&Practice/comments.cpp
@@ -0,0 +1,11 @@
+/*
+Author: [Abu Bakar](https://github.com/HasanBakar)
+📅 date: 15 September 2024 || Day 2
+*/ 
+#include<iostream>
+using namespace std;
+int main(){
+    // print hello world on screen
+    cout << "Hello, World!" << endl;
+    return 0;
+}
